d67887302093f3bf178b5db13570887ef8fff09b
chris-stanton50 <chris.stanton50@outlook.com>
2025-01-22 01:15:35 +0000

    include optional kwarg for filtering and offline smoothing

--- scripts/test_run_scripts.sh
+++ scripts/test_run_scripts.sh
@@ -4,14 +4,18 @@
 arg1_values=("ts_mv_ou" "mv_ou" "iou" "ibm")
 arg2_values=("-f" "-s" "-os")
 arg3_values=("-test")
+arg4_values=("10" "20" "30")
+
 
 # Loop through all possible combinations of arguments
 for arg1 in "${arg1_values[@]}"; do
     for arg2 in "${arg2_values[@]}"; do
         for arg3 in "${arg3_values[@]}"; do
-            echo "Running with arguments: $arg1 $arg2 $arg3"
-            python3 parallel_filt_smth.py -local "$arg1" "$arg2" "$arg3"
-            echo ""
+            for arg4 in "${arg4_values[@]}"; do
+                echo "Running with arguments: $arg1 $arg2 $arg3"
+                python3 parallel_filt_smth.py -local "$arg1" "$arg2" "$arg3" "$arg4" 
+                echo ""
+            done
         done
     done
 done
