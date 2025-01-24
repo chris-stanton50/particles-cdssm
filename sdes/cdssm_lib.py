ea16f94bdc10f464d8e753edc1e1aca15f76d7d4
chris-stanton50 <chris.stanton50@outlook.com>
2025-01-24 00:39:07 +0000

    change default_T to 50

--- sdes/cdssm_lib.py
+++ sdes/cdssm_lib.py
@@ -64,7 +64,7 @@ MV_OU = {
                     'G': np.eye(2),
                     'covY': (0.06 ** 2) * np.eye(2)
                     },
-    'default_T': 10,
+    'default_T': 50,
     'seed': 34953,
     'fk_names': ['BsR_DH', 'BwR_DH_OUP', 'BwR_DH_NDBBrP', 'FwR_DH_OUP', 'FwR_DH_DBrP', 'FwR_DH_NDBBrP']
     }
