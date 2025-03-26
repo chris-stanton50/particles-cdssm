a587c6b52a8ebea5305a5d7c6b2f08e640200b8f
chris-stanton50 <chris.stanton50@outlook.com>
2025-03-26 14:51:55 +0000

    add API tests

---
+++ tests/cdssm_particle_history_test.py
@@ -0,0 +1,41 @@
+from sdes.cdssm_lib import CDSSM_LIB, build_cdssm
+from sdes.core import CDSSM_SMC
+import sdes.feynman_kac as sfk
+
+T = 5; N=10; num=10; M=5
+
+# Build the cdssm
+cdssm_str = 'ou'
+cdssm_spec = CDSSM_LIB[cdssm_str]
+cdssm = build_cdssm(cdssm_str, True)
+
+# Simulate synthetic data from the cdssm
+x, y = cdssm.simulate(T)
+
+# Generate all possible fk models for the given cdssm
+all_fks = sfk.gen_all_fk_models(cdssm, y, smoothing=False)
+all_smth_fks = sfk.gen_all_fk_models(cdssm, y, smoothing=True)
+
+# Run backward_sampling_genealogy for all filtering fks
+filt_algs = {}
+for fk_name, fk in all_fks.items():
+    filt_algs[fk_name] = CDSSM_SMC(fk=fk, N=100, store_history=True)
+    
+for fk_name, smc in filt_algs.items():
+    smc.run()
+    print(f'Running backward_sampling_genealogy for {fk_name}')
+    samples = smc.hist.backward_sampling_genealogy(M)
+
+# Run `backward_sampling_` + genealogy, ON2 and mcmc for all smoothing fks
+smth_algs = {}
+for fk_name, fk in all_smth_fks.items():
+    smth_algs[fk_name] = CDSSM_SMC(fk=fk, N=100, num=10, store_history=True)    
+
+for fk_name, smc in smth_algs.items():
+    smc.run()
+    print(f'Running backward_sampling_genealogy for {fk_name}')
+    samples_g = smc.hist.backward_sampling_genealogy(M)
+    print(f'Running backward_sampling_ON2 for {fk_name}')
+    samples_o = smc.hist.backward_sampling_ON2(M)
+    print(f'Running backward_sampling_mcmc for {fk_name}')
+    samples_m = smc.hist.backward_sampling_mcmc(M)
---
+++ tests/smoothing_worker_test.py
@@ -0,0 +1,24 @@
+from sdes.cdssm_lib import CDSSM_LIB, build_cdssm
+from sdes.core import CDSSM_SMC
+import sdes.feynman_kac as sfk
+from particles.utils import multiplexer
+from sdes.core import CDSSM_SMC, smoothing_worker
+
+Ns=[5, 10, 20]; T = 5; nums = [10, 20]
+smth_methods = ['FFBS_ON2', 'FFBS_MCMC', 'genealogy']
+
+# Build the cdssm
+cdssm_str = 'ou'
+cdssm_spec = CDSSM_LIB[cdssm_str]
+cdssm = build_cdssm(cdssm_str, smoothing=True)
+
+# Simulate synthetic data from the cdssm
+x, y = cdssm.simulate(T)
+
+# Generate all possible fk models for the given cdssm
+all_smth_fks = sfk.gen_all_fk_models(cdssm, y, smoothing=True)
+    
+# Run smoothing worker with multiplexer for all the fk models
+# Note: If different add_funcs are set instead of the default choices, then 
+# the add_funcs need to be passed as `protected_args`  to the multiplexer function.
+cdssm_output = multiplexer(f=smoothing_worker, nruns=3, nprocs=0, seeding=True, method=smth_methods, N=Ns, fk=all_smth_fks, num=nums, smc_cls=CDSSM_SMC)
